062177eb791dd592474c27825fcaea9a5a5dc205
lubomira.trnavska <lubomira.trnavska@softpoint.sk>
2020-04-15 21:30:42 +0200

    collecting_output_evaluation

--- masks.py
+++ masks.py
@@ -1,32 +1,93 @@
-import math
 import numpy as np
+from utilities import *
 
 
-def collect_output(model, img, stride, patch_size=128):
-    patch_sums = np.zeros(img.shape)
-    patch_counts = np.zeros(img.shape)
+def collect_output(model, img, stride=128, patch_size=128):
+    patch_sums = np.zeros((img.shape[0], img.shape[1]))
+    patch_counts = np.zeros((img.shape[0], img.shape[1]))
 
-    last_patch_vert = img.shape[0] - patch_size
-    last_stride_vert = math.ceil((last_patch_vert) / stride) * stride
-    last_patch_horiz = img.shape[1] - patch_size
-    last_stride_horiz = math.ceil((last_patch_horiz) / stride) * stride
+    last_patch_vert, last_stride_vert, last_patch_horiz, last_stride_horiz = find_last_patch_indices(img, stride)
 
     i = 0
-    
-    while i + 128 <= img.shape[0]:
+    while i + patch_size <= img.shape[0]:
         j = 0
-        while j + 128 <= img.shape[1]:
-            
+        while j + patch_size <= img.shape[1]:
+
             crop = img[i: patch_size + i, j: patch_size + j]
-            predicted = model.predict(np.expand_dims(crop, axis = 0))
-            
-            patch_sums[i: patch_size + i, j: patch_size + j]  += predicted[0]
-            patch_counts[i: patch_size + i, j: patch_size + j]  += 1 
+            predicted = model.predict(np.expand_dims(crop, axis=0))
+
+            patch_sums[i: patch_size + i, j: patch_size + j] += np.squeeze(predicted)
+            patch_counts[i: patch_size + i, j: patch_size + j] += 1
 
             j = j + stride
             if j == last_stride_horiz: j = last_patch_horiz
 
         i = i + stride
         if i == last_stride_vert: i = last_patch_vert
+
+    return patch_sums / patch_counts
+
+
+def collect_output_median(model, img, stride=128, patch_size=128, process_predicted_patch=None, predict_patch=None):
+    predictions = np.ones((img.shape[0], img.shape[1], 1))*(-1)
+    predictions = predictions.tolist()
+
+    last_patch_vert, last_stride_vert, last_patch_horiz, last_stride_horiz = find_last_patch_indices(img, stride)
+
+    i = 0
+    while i + patch_size <= img.shape[0]:
+        j = 0
+        while j + patch_size <= img.shape[1]:
+
+            crop = img[i: patch_size + i, j: patch_size + j]
+            predicted = predict_patch(model, crop) if predict_patch else np.squeeze(model.predict(np.expand_dims(crop, axis=0)))
+            if process_predicted_patch: predicted = process_predicted_patch(predicted)
+
+            for row in range(i, i + patch_size):
+                for col in range(j, j + patch_size): 
+                    predictions[row][col].append(predicted[row-i][col-j]) 
+
+            j = j + stride
+            if j == last_stride_horiz: j = last_patch_horiz
+
+        i = i + stride
+        if i == last_stride_vert: i = last_patch_vert
+            
+    result = np.zeros((img.shape[0], img.shape[1]))
     
-    return patch_sums/patch_counts
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            result[i][j] = np.median(predictions[i][j][1:])
+    
+    return result
+
+
+def median_from_rotations(model, img):
+    to_predict = rotate(img)
+    predicted = model.predict(to_predict, verbose=0)
+    back_rot = rotate_back(predicted)
+    med = np.median(back_rot, axis=0)
+    
+    return np.squeeze(med)
+
+
+def otsu_theshold(prediction):
+    _prediction = np.squeeze(prediction * 255)
+    _prediction = _prediction.astype(dtype='uint8')
+    _, mask = cv2.threshold(_prediction, 0, 255, cv2.THRESH_OTSU)
+    
+    return mask
+
+
+def constant_threshold(preditction, threshold=127):
+    _prediction = np.squeeze(prediction * 255)
+    _prediction = _prediction.astype(dtype='uint8')
+    _, mask = cv2.threshold(_prediction, 0, threshold, cv2.THRESH_BINARY)
+    
+    return mask
+
+
+
+    
+
+            
--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import math
 
 def rotation_names():
     return ['', '_rot90', '_rot180', '_rot270', '_flip', '_flip_rot90', '_flip_rot_180', '_flip_rot270']
@@ -17,6 +17,7 @@ def rotate(img):
     result[5] = cv2.rotate(temp, cv2.ROTATE_90_CLOCKWISE)
     result[6] = cv2.rotate(temp, cv2.ROTATE_180)
     result[7] = cv2.rotate(temp, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    
     return result
 
 
@@ -31,4 +32,14 @@ def rotate_back(rotated_images):
     result[5] = cv2.flip(cv2.rotate(imgs[5], cv2.ROTATE_90_COUNTERCLOCKWISE), 0)
     result[6] = cv2.flip(cv2.rotate(imgs[6], cv2.ROTATE_180), 0)
     result[7] = cv2.flip(cv2.rotate(imgs[7], cv2.ROTATE_90_CLOCKWISE), 0)
+    
     return result
+
+
+def find_last_patch_indices(img, stride, patch_width=128, patch_height=128):
+    last_patch_vert = img.shape[0] - patch_height
+    last_stride_vert = math.ceil((last_patch_vert) / stride) * stride
+    last_patch_horiz = img.shape[1] - patch_width
+    last_stride_horiz = math.ceil((last_patch_horiz) / stride) * stride
+    
+    return last_patch_vert, last_stride_vert, last_patch_horiz, last_stride_horiz
